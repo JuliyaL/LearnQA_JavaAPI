cc3333f96170602c26700847b59074970978f438
JuliyaL <yulogacheva@gmail.com>
2023-10-04 23:00:24 +0700

    Ex3,Ex4

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,7 +1,14 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class HelloWorldTest {
     @Test
     public void testHelloWorld (){
-        System.out.print("Hello world!");
+   System.out.println("Hello from Juliya!");
+    Response response = RestAssured
+            .get ("https://playground.learnqa.ru/api/get_text")
+        .andReturn();
+    response.prettyPrint();
             }
 }
