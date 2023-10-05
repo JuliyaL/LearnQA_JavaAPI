2a2c25705d918b903512b1b8058f32798e6e0dc3
JuliyaL <yulogacheva@gmail.com>
2023-10-06 01:12:40 +0700

    Ex6

--- src/test/java/SecondLessonTest.java
+++ src/test/java/SecondLessonTest.java
@@ -22,4 +22,19 @@ public class SecondLessonTest {
 
     }
 
+    @Test
+    public void testRestAssuredEx6(){
+        System.out.println("Ex6:");
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get ("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader=response.getHeader("Location");
+        System.out.println(locationHeader);
+            }
+
 }
