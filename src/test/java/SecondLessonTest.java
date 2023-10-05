22e23c2d0f5585ddcafda919e58260b88df17606
JuliyaL <yulogacheva@gmail.com>
2023-10-06 00:46:21 +0700

    Ex5

---
+++ src/test/java/SecondLessonTest.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class SecondLessonTest {
+    @Test
+    public void testRestAssuredEx5(){
+        System.out.println("Ex5:");
+        Response response = RestAssured
+                .get ("https://playground.learnqa.ru/api/get_json_homework")
+                .andReturn();
+       System.out.println("Первый вариант с помощью Map:");
+       Map<String, String> message = response.jsonPath().getMap("messages[1]");
+       System.out.println(message.get("message"));
+
+       System.out.println("Второй вариант с помощью List:");
+       List<Map<String, String>> messages = response.jsonPath().getList("messages");
+       System.out.println(messages.get(1).get("message"));
+
+    }
+
+}
