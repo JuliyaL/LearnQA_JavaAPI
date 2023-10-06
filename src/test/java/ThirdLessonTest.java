dc787a193c1c9ee0cacaa7bf145579cdd5bbd8d6
JuliyaL <yulogacheva@gmail.com>
2023-10-06 20:34:53 +0700

    Ex11

--- src/test/java/ThirdLessonTest.java
+++ src/test/java/ThirdLessonTest.java
@@ -1,13 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ThirdLessonTest {
     @ParameterizedTest
     @ValueSource(strings = {"Осень", "Осень-это сны листопада..."})
     public void Ex10(String strExample){
-        System.out.println("Ex10:");
         int strLength = strExample.length();
         assertTrue(strLength >15, "Тест провален. Длина строки меньше 15 символов:"+strExample);
+    }
+    @Test
+    public void Ex11(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        String cookie = response.getCookies().toString();
+        //System.out.println("cookie:"+cookie);
+        assertEquals("{HomeWork=hw_value}", cookie, "Неправильные cookie:"+cookie);
     }
 }
